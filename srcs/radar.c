433f328a7b13fad6ed43ed301ac1281f76dedf4d
Cedric BARBIER <cbarbier@e2r4p13.42.fr>
2017-09-20 17:07:18 +0200

    player on minimap

--- srcs/radar.c
+++ srcs/radar.c
@@ -6,13 +6,13 @@
 /*   By: cbarbier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/20 10:03:21 by cbarbier          #+#    #+#             */
-/*   Updated: 2017/09/20 11:52:55 by cbarbier         ###   ########.fr       */
+/*   Updated: 2017/09/20 16:14:17 by cbarbier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
-static int	draw_case(t_w3d *e, int x, int y)
+static int	draw_case(t_w3d *e, int x, int y, unsigned int c)
 {
 	int			i;
 	int			j;
@@ -24,10 +24,7 @@ static int	draw_case(t_w3d *e, int x, int y)
 		i = 0;
 		while (i < 4)
 		{
-			if ((int)e->pos.x == i && (int)e->pos.y == y)
-				put_pxl_img(e, x + i, y + j, 0x00FFFF);
-			else
-				put_pxl_img(e, x + i, y + j, 0xFFFFFF);
+			put_pxl_img(e, x + i, y + j, c);
 			i++;
 		}
 		j++;
@@ -35,18 +32,18 @@ static int	draw_case(t_w3d *e, int x, int y)
 	return (1);
 }
 
-static int	draw_line(t_w3d *e, int *map, int w, int y)
+static int	draw_line(t_w3d *e, int *map, int yp)
 {
 	int		i;
-	int		x;
+	int		xp;
 
-	x = 5;
+	xp = 5;
 	i = -1;
-	while (i <= w)
+	while (i <= e->width)
 	{
-		if (i < 0 || i == w || !map || map[i])
-			draw_case(e, x, y);
-		x += 4;
+		if (i < 0 || i == e->width || !map || map[i])
+				draw_case(e, xp, yp, 0xDD00CC);
+		xp += 4;
 		i++;
 	}
 	return (1);
@@ -55,18 +52,17 @@ static int	draw_line(t_w3d *e, int *map, int w, int y)
 int			draw_radar(t_w3d *e)
 {
 	int			**map;
-	int			h;
-	int			y;
+	int			yp;
 
-	h = e->height;
 	map = e->map;
-	y = 10;
-	draw_line(e, 0, e->width, 6);
+	yp = 10;
+	draw_line(e, 0, 6);
 	while (*map)
 	{
-		draw_line(e, *map++, e->width, y);
-		y += 4;
+		draw_line(e, *map++, yp);
+		yp += 4;
 	}
-	draw_line(e, 0, e->width, y);
+	draw_line(e, 0, yp);
+	draw_case(e, 10 + (int)e->pos.y * 4, 9 + (int)e->pos.x * 4, 0xEEEEEE);
 	return (1);
 }
